7fb3e92bb49cfbc2c9a91f1ccda494619e2f1f5e
goscen <anar.guseynov.03@mail.ru>
2022-09-27 20:07:17 +0500

    telegramBot update

--- src/main/java/org/example/TelegramBot.java
+++ src/main/java/org/example/TelegramBot.java
@@ -1,22 +1,44 @@
 package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class TelegramBot extends TelegramLongPollingBot {
 
+    private final String botName;
+    private final String botToken;
+
+    TelegramBot(String botName, String botToken) {
+        this.botName = botName;
+        this.botToken = botToken;
+
+    }
+
     @Override
     public String getBotUsername() {
-        return null;
+
+        return botName;
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return botToken;
     }
-
     @Override
     public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
 
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
